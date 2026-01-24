eeab131e6b334673ad13d09790bf12878f50ad6a
Fireman9143 <168347053+Fireman9143@users.noreply.github.com>
2026-01-24 15:37:54 -0600

    Add files via upload

--- ClearCodeFarming/scenes/UI/plant_info.gd
+++ ClearCodeFarming/scenes/UI/plant_info.gd
@@ -9,12 +9,10 @@ func setup(source):
 	$HBoxContainer/VBoxContainer/GrowthBar.max_value = plant.max_age
 	$HBoxContainer/VBoxContainer/GrowthBar.max_value = plant.death_max
 	update()
-	
+	plant.died.connect(update)
 	
 func update():
-	plant.died.connect(death_check)
 	$HBoxContainer/VBoxContainer/GrowthBar.value = plant.age
 	$HBoxContainer/VBoxContainer/DeathBar.value = plant.death_count
-
-func death_check():
-	queue_free()
+	if plant.death_count >= plant.death_max:
+		queue_free()
